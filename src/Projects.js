f54b9433769100d15f3b8d36ce0fadf9e4289997
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-17 18:50:46 -0500

    wind

--- src/Projects.js
+++ src/Projects.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Projects = () => {
   return (
     <div>
-      <a href="#">Project 1</a>
-      <a href="#">Project 2</a>
+      <a href="/project1">Project 1</a>
+      <a href="/project2">Project 2</a>
       {/* ... other projects */}
     </div>
   );
