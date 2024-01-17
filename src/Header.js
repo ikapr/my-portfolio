2049651413998ade4fd94bca03423a710e0da152
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-17 16:52:58 -0500

    fc

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Isaiah Apreku</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+};
+
+export default Header;
