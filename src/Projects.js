c9d3859f7aa813fea2c768f699227d242be35fb8
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-17 18:44:20 -0500

    fc

--- src/Projects.js
+++ src/Projects.js
@@ -3,28 +3,11 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <div className="project-card">
-        <h3>Project 1</h3>
-        <p>
-          soon
-        </p>
-        <a href="#">View Project</a>
-        <a href="#">GitHub Repository</a>
-      </div>
-
-      <div className="project-card">
-        <h3>Project 2</h3>
-        <p>
-         soon
-        </p>
-        <a href="#">View Project</a>
-        <a href="#">GitHub Repository</a>
-      </div>
-
-      {/* Add more project cards as needed */}
-    </section>
+    <div>
+      <a href="#">Project 1</a>
+      <a href="#">Project 2</a>
+      {/* ... other projects */}
+    </div>
   );
 };
 
