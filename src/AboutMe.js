dcc39d60865abd64686cb761062dd562f85f6002
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-17 17:52:12 -0500

    sc

---
+++ src/AboutMe.js
@@ -0,0 +1,17 @@
+// AboutMe.js
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about-me">
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Isaiah A! I'm a passionate web developer with experience in building modern and responsive web applications.
+        My skills include HTML, JAVASCRIPT, SQL, and React. I enjoy tackling challenges and constantly learning new technologies.
+        Let's collaborate and create something amazing together!
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
--- src/App.js
+++ src/App.js
@@ -22,5 +22,31 @@ function App() {
   );
 }
 
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Header from './header';
+import AboutMe from './AboutMe';
+import Projects from './Projects';
+import Contact from './Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar /> {/* Include the NavBar component */}
+        <Route path="/" exact component={Header} />
+        <Route path="/about" component={AboutMe} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+};
+
 export default App;
+
+
+
 
