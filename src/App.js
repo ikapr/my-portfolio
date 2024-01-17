5848108718564c188a6d7d4e341a5462baeb5979
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-17 18:17:48 -0500

    appc

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './NavBar';
-import Header from './header';
+import Header from './header';  // Assuming 'header' is the correct case for your component
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Contact from './Contact';
@@ -46,7 +21,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
